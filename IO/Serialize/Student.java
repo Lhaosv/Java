5ec52648ab77067a68e4b8565e0ad8008bd8d68e
Lhaosv <823062559@qq.com>
2021-07-17 17:37:28 +0800

    序列化与反序列化

--- IO/Serialize/Student.java
+++ IO/Serialize/Student.java
@@ -1,10 +1,16 @@
 package IO.Serialize;
 
 import java.io.Serializable;
-
+/*
+实现了 Serializable 的类建议手动生成序列号版本号
+ */
 public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;  //鼠标指向类名， ALT + 回车 生成序列化版本号
     private int num;
     private String name;
+    //一下代码再序列化之后进行了修改，依然可以反序列化
+    public int age;
+
 
     public Student() {
     }
